eddf14d33b97053ea272567f94296799881700ee
YIRU CANG <cang.y@husky.neu.edu>
2018-05-08 11:24:03 +0800

    test company

--- src/split/getMatch.java
+++ src/split/getMatch.java
@@ -5,6 +5,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,6 +15,8 @@ public class getMatch {
 		try{
 			String encoding = "UTF-8";
 			File file = new File("E.txt");
+			List <String> id = new ArrayList<>();
+			List <String> information = new ArrayList<>();
 			
 			if (file.isFile() && file.exists()){
 				InputStreamReader read = new InputStreamReader(new FileInputStream(file), encoding);
@@ -32,34 +36,50 @@ public class getMatch {
                 for(String s: splited){
                 	if(i%2!=0){
                 	//System.out.println(s);
+                		id.add(s);
                 	}else{
-                		Pattern p1 = Pattern.compile(".*公司.*");
-                		Matcher comp = p1.matcher(s);
+                		
+                		String subs[] = s.split("\n");
+                		String compSet = "NA";
+                		
+                		for(String ss: subs){
+                	    Pattern p1 = Pattern.compile(".*公司.*");
+                		Matcher comp = p1.matcher(ss);
                 		boolean result2 = comp.find();
                 		String company_result = null;
                 		
                 		if (result2)
                         {
-                            company_result=comp.group(0);
-                            //str.add(company_result);
+                            company_result=comp.group(0);                   
+                           // strs.add(company_result);                   
                         }
-                		//System.out.println(s);
-                	}
+                		if(company_result!=null){
+                		compSet = compSet+company_result;
+                		}
+                		}
+                		if(compSet.contains("抱歉哦亲， 您询价的产品还没有在这边销售")){
+                			compSet = compSet.replaceAll("抱歉哦亲， 您询价的产品还没有在这边销售， 所以我无法给您报价，您需要采购的话可以帮您转给西门子线下渠道报价，需要提供下您的公司名称和联系电话","NA");
+                			}
+                		information.add(compSet);
+                    }
                 	i=i+1;
-                }
-                //System.out.println(splited[18]);
-                
+                }                
 			}
 			else
             {
                 System.out.println("找不到指定的文件");
             }
 			
+			information.remove(0);
 			
+			
+			System.out.println(id);
+			System.out.println(id.size());
+			System.out.println(information)	;
+			System.out.println(information.size())	;
 		}catch(Exception e){
 			System.out.println("读取文件内容出错");
             e.printStackTrace();
 		}
-		
 	}
 }
