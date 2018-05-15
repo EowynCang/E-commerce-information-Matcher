3f8e4e51db07938f084f64a7e5e97db3e11aa063
YIRU CANG <cang.y@husky.neu.edu>
2018-05-15 17:12:14 +0800

    update date

--- src/split/getMatch.java
+++ src/split/getMatch.java
@@ -31,6 +31,7 @@ public class getMatch {
 			File file = new File("E.txt");
 			List<String> id = new ArrayList<>();
 			List<String> information = new ArrayList<>();
+			List<String> date_info = new ArrayList<>();
 			
 
 			// The account responsible for the purchase
@@ -58,14 +59,26 @@ public class getMatch {
 					} else {
 
 						String subs[] = s.split("\n");
-						String compSet = "NA";
+						String compSet = "";
+						
+						
+						Pattern p2 = Pattern.compile("20[0-9][0-9]-[0-1][0-9]-[0-3][0-9]");
+						Matcher date = p2.matcher(s);
+						boolean date_r = date.find();
+						String date_result = null;
+						
+						if(date_r){
+							date_result = date.group(0);
+							date_info.add(date_result);
+						}
 
 						for (String ss : subs) {
 							Pattern p1 = Pattern.compile(".*公司.*");
 							Matcher comp = p1.matcher(ss);
 							boolean result2 = comp.find();
 							String company_result = null;
-
+							
+							
 							if (result2) {
 								company_result = comp.group(0);
 								// strs.add(company_result);
@@ -76,8 +89,10 @@ public class getMatch {
 						}
 						if (compSet.contains("抱歉哦亲， 您询价的产品还没有在这边销售")) {
 							compSet = compSet.replaceAll(
-									"抱歉哦亲， 您询价的产品还没有在这边销售， 所以我无法给您报价，您需要采购的话可以帮您转给西门子线下渠道报价，需要提供下您的公司名称和联系电话", "NA");
+									"抱歉哦亲， 您询价的产品还没有在这边销售， 所以我无法给您报价，您需要采购的话可以帮您转给西门子线下渠道报价，需要提供下您的公司名称和联系电话", "");						
 						}
+						compSet = compSet.replaceAll("您需要采购的话可以帮您转给西门子线下渠道报价，需要提供下您的公司名称和联系电话", "");
+
 						information.add(compSet);
 					}
 					i++;
@@ -87,19 +102,21 @@ public class getMatch {
 			}
 
 			information.remove(0);
-			writeFile(id, information, 1, "writeExcel.xlsx");
+			writeFile(id, information,date_info, 1, "writeExcel.xlsx");
 
 			System.out.println(id);
 			System.out.println(id.size());
 			System.out.println(information);
 			System.out.println(information.size());
+			System.out.println(date_info);
+			System.out.println(date_info.size());
 		} catch (Exception e) {
 			System.out.println("Warning ! Reading file error!");
 			e.printStackTrace();
 		}
 	}
 
-	private static void writeFile(List<String> idList, List<String> infoList, int cloumnCount, String finalXlsxPath) {
+	private static void writeFile(List<String> idList, List<String> infoList,List<String> date_info, int cloumnCount, String finalXlsxPath) {
 
 		OutputStream out = null;
 		try {
@@ -138,14 +155,28 @@ public class getMatch {
 				String i = infoList.get(j);
 				String info = i.toString();
 				
+				String d = date_info.get(j);
+				String dateInfo = d.toString();
+				
 				for (int k = 0; k <= columnNumCount; k++) {
 					
 					// Loop in one row
 					Cell first = row.createCell(0);
-					first.setCellValue(idInfo);
+					first.setCellValue(j+1);
 					
 					Cell second = row.createCell(1);
-					second.setCellValue(info);
+					second.setCellValue("Alibaba");
+					
+					Cell third =row.createCell(2);
+					third.setCellValue(idInfo);
+					
+					Cell seventh =row.createCell(6);
+					seventh.setCellValue(dateInfo);
+					
+					Cell eighth =row.createCell(8);
+					eighth.setCellValue(info);
+					
+
 				}
 			}
 
