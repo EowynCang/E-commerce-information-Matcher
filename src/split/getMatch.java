f41c6d78c442f3b6f05b00411e22255723ad712d
YIRU CANG <cang.y@husky.neu.edu>
2018-05-31 11:43:24 +0800

    change brs

--- src/split/getMatch.java
+++ src/split/getMatch.java
@@ -27,7 +27,7 @@ public class getMatch {
 	private static final String BRS = "转出BRS";
 	private static final String DISTRIBUTOR = "推荐分销商";
 	private static final String UNSETTLED = "Eshop产品未成交";
-	private static final String RECORD_FILE_NAME = "eshopali123123.txt";
+	private static final String RECORD_FILE_NAME = "eshop456.txt";
 	private static final String REGION_FILE_NAME = "region.xls";
 	private static final String CHANNEL = "Alibaba";
 	private static final String BU_FA = "FA";
@@ -35,6 +35,18 @@ public class getMatch {
 	private static final String BU_MC = "MC";
 	private static final String BU_CS = "CS";
 	private static final String BU_CP = "CP";
+	private static final String REGION_DEFINED_RN = "收到，帮您转出";
+	private static final String REGION_DEFINED_RS = "好的，帮您转出了";
+	private static final String REGION_DEFINED_RNE = "好的，那就帮您转出";
+	private static final String REGION_DEFINED_RW = "收到，那帮您转出";
+	private static final String REGION_DEFINED_RC = "收到，已经帮您转出";
+	private static final String REGION_DEFINED_RE = "嗯，帮您加急转出";
+	private static final String REGION_RN = "RN";
+	private static final String REGION_RS = "RS";
+	private static final String REGION_RNE = "RNE";
+	private static final String REGION_RW = "RW";
+	private static final String REGION_RC = "RC";
+	private static final String REGION_RE = "RE";
 
 	public static void main(String[] args) throws FileNotFoundException {
 		try {
@@ -50,6 +62,7 @@ public class getMatch {
 			List<String> bu = new ArrayList<>();
 			List<String> product = new ArrayList<>();
 			List<String> dataOut = new ArrayList<>();
+			List<String> brsManager = new ArrayList<>();
 
 			//Delete the last character test
 //			String a[] = {"a","b","c","d"};
@@ -71,9 +84,9 @@ public class getMatch {
 				BufferedReader bufferedReader = new BufferedReader(read);
 				String lineTxt = null;
 				String all = "";
+				
 				localHost = bufferedReader.readLine();
 				localHost = filterHost(localHost);
-
 				// System.out.println(localHost);
 				// System.out.println("localHost 长度："+localHost.length());
 
@@ -102,6 +115,8 @@ public class getMatch {
 						matchStatus(sub, status,dataOut,localHost);
 
 						matchProductBu(sub, product, bu);
+						
+						matchRegion(sub, region);
 
 					}
 					i++;
@@ -113,10 +128,14 @@ public class getMatch {
 			information.remove(0);
 			phone.remove(0);
 			status.remove(0);
+			region.remove(0);
 			product.remove(0);
 			bu.remove(0);
 			dataOut.remove(0);
-			writeFile(id, information, date_info, phone, status,dataOut, bu, product, 1, "writeExcel.xlsx");
+			
+			
+			matchBrsManager(region,bu,brsManager);
+			writeFile(region,id, information, date_info, phone, status,dataOut, bu, product, 1, "writeExcel.xlsx");
 
 			try {
 				System.out.println(id);
@@ -135,18 +154,8 @@ public class getMatch {
 				e.printStackTrace();
 			}
 
-			dataQuery dq = new dataQuery();
-			File read_file = new File(REGION_FILE_NAME);
-			List excelList = dq.readExcel(read_file);
 
-			for (int i = 0; i < excelList.size(); i++) {
-				List list = (List) excelList.get(i);
-				for (int j = 0; j < list.size(); j++) {
-					// System.out.print(list.get(j));
-				}
-				// System.out.println();
-			}
-			System.out.println(excelList);
+			readRegion();
 
 		} catch (Exception e) {
 			System.out.println("Warning ! Reading file error!");
@@ -154,6 +163,25 @@ public class getMatch {
 		}
 	}
 
+	private static void matchBrsManager(List<String> region,List<String> bu,List<String> brsManager){
+		
+	}
+	
+	private static void readRegion(){
+		dataQuery dq = new dataQuery();
+		File read_file = new File(REGION_FILE_NAME);
+		List excelList = dq.readExcel(read_file);
+
+		for (int i = 0; i < excelList.size(); i++) {
+			List list = (List) excelList.get(i);
+			for (int j = 0; j < list.size(); j++) {
+				// System.out.print(list.get(j));
+			}
+			// System.out.println();
+		}
+		System.out.println(excelList);
+	}
+	
 	private static String filterHost(String localHost) {
 		Pattern p1 = Pattern.compile("[a-zA-Z0-9].*");
 		Matcher m = p1.matcher(localHost);
@@ -165,7 +193,7 @@ public class getMatch {
 		return localHost;
 	}
 
-	private static void writeFile(List<String> idList, List<String> infoList, List<String> date_info,
+	private static void writeFile(List<String> region,List<String> idList, List<String> infoList, List<String> date_info,
 			List<String> phone, List<String> status,List<String> dataOut, List<String> bu, List<String> product, int cloumnCount,
 			String finalXlsxPath) {
 
@@ -226,6 +254,9 @@ public class getMatch {
 				
 				String o = dataOut.get(j);
 				String dataOutInfo = o.toString();
+				
+				String r = region.get(j);
+				String regionInfo = r.toString();
 
 				for (int k = 0; k <= columnNumCount; k++) {
 
@@ -255,10 +286,14 @@ public class getMatch {
 					eighth.setCellValue(productInfo);
 
 					Cell ninth = row.createCell(8);
+					if(info.contains(phoneInfo)){
 					ninth.setCellValue(info);
+					}else{
+						ninth.setCellValue(info+" "+phoneInfo);
+					}
 
 					Cell tenth = row.createCell(9);
-					tenth.setCellValue(phoneInfo);
+					tenth.setCellValue(regionInfo);
 
 				}
 			}
@@ -318,6 +353,14 @@ public class getMatch {
 			    Pattern p1 = Pattern.compile("\\):.+工贸.*|\\):.+公司.*|\\):.+学校.*|\\):.+科技.*|\\):.+自动化.*");
 				Matcher comp = p1.matcher(ss);
 				boolean result2 = comp.find();
+				
+				Pattern p = Pattern.compile("1[0-9]{10}");
+				Matcher m = p.matcher(ss);
+				boolean result = m.find();
+				String phone_result = null;
+				
+				
+				
 				if(!result2){
 					p1 = Pattern.compile(".{13}公司.*|.{13}工贸.*|.{13}学校.*|.{13}科技.*|.{13}自动化.*");
 					comp = p1.matcher(ss);
@@ -409,13 +452,32 @@ public class getMatch {
 			}
 		}
 		distributorSet = distributorSet.replaceAll("为您推荐官方授权代理商：","");
+		distributorSet = distributorSet.replaceAll("为您推荐官方授权代理商 ：","");
 		distributorSet = distributorSet.replaceAll("联系下这个人","");
 		status.add(statusSet);
 		dataOut.add(distributorSet);
 	}
 
-	private static void matchRegion(String s, List<String> region) {
+	private static void matchRegion(String subs[], List<String> region) {
 
+		String regionSet = "";
+		
+		for (String ss : subs) {
+			if(ss.contains(REGION_DEFINED_RS)){
+				regionSet = REGION_RS;
+			}else if(ss.contains(REGION_DEFINED_RN)){
+				regionSet = REGION_RN;
+			}else if(ss.contains(REGION_DEFINED_RNE)){
+				regionSet = REGION_RNE;
+			}else if(ss.contains(REGION_DEFINED_RW)){
+				regionSet = REGION_RW;
+			}else if(ss.contains(REGION_DEFINED_RC)){
+				regionSet = REGION_RC;
+			}else if(ss.contains(REGION_DEFINED_RE)){
+				regionSet = REGION_RE;
+			}
+		}
+		region.add(regionSet);
 	}
 
 	private static void matchProductBu(String subs[], List<String> product, List<String> bu) {
@@ -428,21 +490,21 @@ public class getMatch {
 				String product_result = null;
 				
 				Pattern pFA = Pattern
-						.compile("6[e|E|A|a][S|s|5|v|V]+[0-9A-Za-z\\-\\s\\()]{0,16}|(触摸屏)|(面板)|(工控机)|[lL][o|O][g|G][o|O]|[c|C][p|P][u|U]|6[e|E][d|D]+[0-9A-Za-z\\-\\s\\()]{0,16}|[p|P][c|C][s|S]7");
+						.compile("6[e|E|A|a][S|s|5|v|V]+[0-9A-Za-z\\-\\s\\()]{0,16}|(触摸屏)|(面板)|(工控机)|[lL][o|O][g|G][o|O]|[c|C][p|P][u|U]|6[e|E][d|D]+[0-9A-Za-z\\-\\s\\()]{0,16}|[p|P][c|C][s|S]7|[s|S][I|i][m|M][A|a][T|t][i|I][C|c]|[t|T][i|I][a|A]|[w|W][I|i][n|N][C|c][c|C]|[p|P][l|L][c|C]|[s|S][M|m][a|A][R|r][T|t]|6[A|a][g|G]+[0-9A-Za-z\\-\\s\\()]{0,16}|[s|S]7\\-(1)?200");
 				Matcher mFA = pFA.matcher(ss);
 				boolean resultFA = mFA.find();
 				
-				Pattern pPA = Pattern.compile("6[e|E][p|P]+[0-9A-Za-z\\-\\s\\()]{0,16}|6[x|X][v|V]+[0-9A-Za-z\\-\\s\\()]{0,16}");
+				Pattern pPA = Pattern.compile("6[e|E][p|P]+[0-9A-Za-z\\-\\s\\()]{0,16}|6[x|X][v|V]+[0-9A-Za-z\\-\\s\\()]{0,16}|6[g|G][t|T|F|f]+[0-9A-Za-z\\-\\s\\()]{0,16}|6[g|G][K|k]7+[0-9A-Za-z\\-\\s\\()]{0,16}");
 				Matcher mPA = pPA.matcher(ss);
 				boolean resultPA = mPA.find();
 
 				Pattern pCP = Pattern.compile(
-						"接触器|断路器|继电器|按钮|指示灯|信号灯|软启|3[R|r|t|T|v|V|u|U][t|v|u|h|b|a|s|d|f|w|g|x|c|k|T|V|U|H|B|A|S|D|F|W|G|X|C|K]+[0-9A-Za-z\\-\\s]{0,16}|8[A-Za-z][A-Za-z]+[0-9A-Za-z\\-\\s]{0,16}|3[s|S][u|U]+[0-9A-Za-z\\-\\s]{0,16}|7[m|M|n|n]+[0-9A-Za-z\\-\\s]{0,16}");
+						"接触器|断路器|继电器|行程开关|按钮|指示灯|信号灯|软启|3[R|r|t|T|v|V|u|U][t|v|u|h|b|a|s|d|f|w|g|x|c|k|T|V|U|H|B|A|S|D|F|W|G|X|C|K]+[0-9A-Za-z\\-\\s]{0,16}|8[A-Za-z][A-Za-z]+[0-9A-Za-z\\-\\s]{8,16}|3[s|S][u|U|b|e|x|y|B|E|X|Y]+[0-9A-Za-z\\-\\s]{0,16}|7[m|M|n|n]+[0-9A-Za-z\\-\\s]{0,16}");
 				Matcher mCP = pCP.matcher(ss);
 				boolean resultCP = mCP.find();
 
 				Pattern pMC = Pattern.compile(
-						"6[F|f|s|S][c|C|x|X|l|L|E|e|K|k]+[0-9A-Za-z\\-\\s]{0,16}|[S|s]120|[V|v][2|9][0|o|O]|[M|m][M|m]4|1[f|g|h|l|k|p|u|F|G|H|L|K|P|U][E|G|K|L|N|S|T|W|D|V|U|M|Y|y|e|g|k|l|n|s|t|w|d|v|u|m][0-9A-Za-z\\-\\s]{0,16}|[g|G]1[1|2]0[a-zA-Z]{0,2}|变频器|数控");
+						"6[F|f|s|S][c|C|x|X|l|L|E|e|K|k]+[0-9A-Za-z\\-\\s]{8,16}|[S|s]120|[V|v][2|9][0|o|O]|[M|m][M|m]4|1[f|g|h|l|k|p|u|F|G|H|L|K|P|U][E|G|K|L|N|S|T|W|D|V|U|M|Y|y|e|g|k|l|n|s|t|w|d|v|u|m][0-9A-Za-z\\-\\s]{9,16}|[g|G]1[1|2]0[a-zA-Z]{0,2}|变频器|数控");
 				Matcher mMC = pMC.matcher(ss);
 				boolean resultMC = mMC.find();
 
@@ -453,6 +515,10 @@ public class getMatch {
 				Pattern pPIC = Pattern.compile("图片");
 				Matcher mPIC = pPIC.matcher(ss);
 				boolean resultPIC = mPIC.find();
+				
+				Pattern pUnknown = Pattern.compile("[1-9][0-9][0-9]\\-[0-9][a-zA-Z][a-zA-Z][0-9][0-9]\\-[0-9][a-zA-Z][a-zA-Z][0-9]");
+				Matcher mUnknown = pUnknown.matcher(ss);
+				boolean resultUnknown = mUnknown.find();
 
 				if(resultFA){
 					product_result = mFA.group(0);
@@ -508,6 +574,19 @@ public class getMatch {
 					product_result = mPIC.group(0);
 					if(!productSet.contains(product_result)){
 						productSet += product_result + "\n";
+					}
+				}
+				
+				
+				if(resultUnknown){
+					product_result = mUnknown.group(0);
+					if(!productSet.contains(product_result)){
+						productSet += product_result + "\n";
+					}
+					if(buSet==""){
+					if(!buSet.contains("*")){
+						buSet += "*";
+					}
 					}
 				}
 				
